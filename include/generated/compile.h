6550988d62536378b6694d11dcae2229d41db667
redcracker <ezhouzhaomin@163.com>
2016-03-12 20:47:51 +0800

    sourcecode init

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 3 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#3 PREEMPT Sat Feb 27 21:10:41 CST 2016"
+#define LINUX_COMPILE_TIME "21:10:41"
+#define LINUX_COMPILE_BY "redcracker"
+#define LINUX_COMPILE_HOST "redcracker-pc"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.8.1 (Sourcery CodeBench Lite 2013.11-33) "
